f304d819fbc2f401692dcfdf60050b95d71dd514
timdavidcole <timdavidcole@gmail.com>
2019-09-02 15:57:54 +0100

    added React basics

--- app/javascript/components/hello_world/PissOffWorld.js
+++ app/javascript/components/hello_world/PissOffWorld.js
@@ -5,7 +5,7 @@ class PissOffWorld extends React.Component {
   render () {
     return (
       <div>
-        Apart from {this.props.specificPerson}, they can piss off.
+        Apart from {this.props.specificPerson}, good DAY to you sir.
       </div>
     );
   }
