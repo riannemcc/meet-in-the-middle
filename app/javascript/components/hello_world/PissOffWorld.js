fecac86a013d842b7382f4073d9490dab3d3f346
timdavidcole <timdavidcole@gmail.com>
2019-09-02 13:52:27 +0100

    added Tims React tutorial

---
+++ app/javascript/components/hello_world/PissOffWorld.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class PissOffWorld extends React.Component {
+  render () {
+    return (
+      <div>
+        Apart from {this.props.specificPerson}, they can piss off.
+      </div>
+    );
+  }
+}
+
+export default PissOffWorld
