6d11b3ce6ef07a0fea7ac832309a6c21b6c0157e
Student <student@Makerss-MacBook-Pro.local>
2019-09-03 09:28:46 +0100

    enzyme setup

---
+++ app/javascript/_tests_/App.tests.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import App from '../App';
+import { create, update } from 'react-test-renderer';
+import '../setupTests';
+import { shallow, mount, render } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
