64b04b8640677325c994d64090ea94359f3639a6
Rianne McCartney <riannemccartney@hotmail.com>
2019-09-03 14:37:04 +0100

    added map markers

--- app/javascript/components/MapContainer.js
+++ app/javascript/components/MapContainer.js
@@ -1,17 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import Geocode from "react-geocode";
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      lat: 51.517432,
+      lng: -0.073262,
+      markers: [{ name: "Test", position: { lat: 51.517432, lng: -0.073262 } }]
+    };
+    this.onMapClicked = this.onMapClicked.bind(this);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onAddItem = this.onAddItem.bind(this);
+  }
+
+  onMapClicked(props, marker, e) {
+    console.log(props);
+    console.log(marker);
+    console.log(e);
+  }
+
+  onMarkerClick(props, marker, e) {
+    console.log(marker);
+  }
+
+  onAddItem(props, marker, e) {
+    console.log(props);
+    console.log(marker);
+    console.log(e.latLng.lat());
+    this.setState(state => {
+      let newMarker = {
+        name: "Test",
+        position: { lat: e.latLng.lat(), lng: e.latLng.lng() }
+      };
+      const markers = [...state.markers, newMarker];
+      return {
+        value: "",
+        lat: 51.517432,
+        lng: -0.073262,
+        markers
+      };
+    });
+  }
+
   render() {
     return (
       <div id="map">
         <Map
           google={this.props.google}
+          onClick={this.onAddItem}
           zoom={18}
           initialCenter={{
-            lat: 51.517432,
-            lng: -0.073262
+            lat: this.state.lat,
+            lng: this.state.lng
           }}
           options={{
             zoomControl: true,
@@ -21,7 +65,24 @@ export class MapContainer extends React.Component {
             rotateControl: false,
             fullscreenControl: false
           }}
-        ></Map>
+        >
+          {this.state.markers.map(
+            function(marker, i) {
+              return (
+                <Marker
+                  onClick={this.onMarkerClick}
+                  key={i}
+                  title={marker.name}
+                  name={marker.name}
+                  position={{
+                    lat: marker.position.lat,
+                    lng: marker.position.lng
+                  }}
+                />
+              );
+            }.bind(this)
+          )}
+        </Map>
       </div>
     );
   }
