0f2d0990f6702e15465df007c3cd333af9b9d067
Will Howes <williameldenhowes@gmail.com>
2019-09-04 10:23:06 +0100

    mock test

--- app/javascript/components/MapContainer.js
+++ app/javascript/components/MapContainer.js
@@ -1,11 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class MapContainer extends React.Component {
+
+  constructor(props) {
+   super(props)
+   this.state = {
+     lat: 51.517432,
+     lng: -0.073262,
+   }
+ }
+
+ findMiddle = (a, b) => {
+
+ }
+
   render () {
     return (
       <div>
       </div>
-    );
+    )
   }
 }
 
